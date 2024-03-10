04647430fcf7779874d9926583f398018a3d01ed
Martín Capuano <capuano.martin@outlook.com>
2024-03-10 12:56:03 -0300

    Empezando de nuevo

--- script.js
+++
@@ -1,2 +0,0 @@
-const WEATHER_URL = "https://api.open-meteo.com/v1/forecast?"
-let restourl="latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=America%2FSao_Paulo"
