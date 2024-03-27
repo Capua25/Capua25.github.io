078f6eccc9699bf2ee7bd09c12a5d8318613621f
Martín Capuano <capuano.martin@outlook.com>
2024-03-27 17:18:18 -0300

    a

--- Astro-V1/astro.config.mjs
+++ Astro-V1/astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://capua25.github.io',
-    base: 'Capua25.github.io'
+    site: 'https://capua25.github.io'
 });
